afdcd195d189fc1ae454bdf5eaba9f1b8807ae5a
Aaron Demers <ad3m3r5@gmail.com>
2019-06-24 05:04:13 -0400

    protocol option

--- index.js
+++ index.js
@@ -1,80 +1,96 @@
 
-const dgram = require('dgram')
-const os = require('os')
+const dgram = require('dgram'); // for udp support
+const net = require('net'); // for tcp support
+const os = require('os');
 
-const winston = require('winston')
+const winston = require('winston');
 
 /* eslint-disable no-empty-function */
 function noop() {}
 /* eslint-enable no-empty-function */
 
 class LogstashTransport extends winston.Transport {
-  constructor(options) {
-    options = options || {}
+    constructor(options) {
+        options = options || {};
 
-    super(options)
+        super(options);
 
-    this.name = 'LogstashTransport'
+        this.name = 'LogstashTransport';
 
-    this.host = options.host
-    this.port = options.port
-    this.trailingLineFeed = options.trailingLineFeed === true
-    this.trailingLineFeedChar = options.trailingLineFeedChar || os.EOL
-    this.silent = options.silent
+        this.host = options.host;
+        this.port = options.port;
+        this.protocol = options.protocol;
+        this.trailingLineFeed = options.trailingLineFeed === true;
+        this.trailingLineFeedChar = options.trailingLineFeedChar || os.EOL;
+        this.silent = options.silent;
 
-    this.client = null
+        this.client = null;
 
-    this.connect()
-  }
-
-  connect() {
-    this.client = dgram.createSocket('udp4')
-    this.client.unref()
-  }
-
-  log(info, callback) {
-    if (this.silent) {
-      return callback(null, true)
+        this.connect();
     }
 
-    this.send(info[Symbol.for('message')], (err) => {
-      this.emit('logged', !err)
-      callback(err, !err)
-    })
-  }
-
-  send(message, callback) {
-    if (this.trailingLineFeed === true) {
-      message = message.replace(/\s+$/, '') + this.trailingLineFeedChar
+    connect() {
+        if (this.protocol.toLowerCase() == 'udp') {
+            this.client = dgram.createSocket('udp4'. () => {
+                console.log(`Connected to ${this.host}:${this.port} over ${this.protocol}`);
+            });
+            this.client.unref();
+        } else { // default to tcp
+            this.client = new net.Socket();
+            this.client.connect(this.port, this.host, () => {
+                console.log(`Connected to ${this.host}:${this.port} over ${this.protocol}`);
+            });
+            this.client.unref();
+        }
     }
 
-    const buf = Buffer.from(message)
-    this.client.send(buf, 0, buf.length, this.port, this.host, (callback || noop))
-  }
+    log(info, callback) {
+        if (this.silent) {
+            return callback(null, true);
+        }
+
+        this.send(info[Symbol.for('message')], (err) => {
+            this.emit('logged', !err);
+            callback(err, !err);
+        });
+    }
+
+    send(message, callback) {
+        if (this.trailingLineFeed === true) {
+            message = message.replace(/\s+$/, '') + this.trailingLineFeedChar;
+        }
+
+        const buf = Buffer.from(message);
+        if (this.protocol.toLowerCase() == 'udp') {
+            this.client.send(buf, 0, buf.length, this.port, this.host, (callback || noop));
+        } else { // default to tcp
+            this.client.write(buf, callback || noop);
+        }
+    }
 }
 
 function createLogger(logType, config) {
-  const appendMetaInfo = winston.format((info) => {
-    return Object.assign(info, {
-      application: logType || config.application,
-      hostname: config.hostname || os.hostname(),
-      pid: process.pid,
-      time: new Date(),
-    })
-  })
-  
-  return winston.createLogger({
-    level: config.level || 'info',
-    format: winston.format.combine(
-      appendMetaInfo(),
-      winston.format.json(),
-      winston.format.timestamp()
-    ),
-    transports: [
-      new LogstashTransport(config.logstash)
-    ].concat(config.transports || [])
-  })
+    const appendMetaInfo = winston.format((info) => {
+        return Object.assign(info, {
+            application: logType || config.application,
+            hostname: config.hostname || os.hostname(),
+            pid: process.pid,
+            time: new Date(),
+        })
+    });
+
+    return winston.createLogger({
+        level: config.level || 'info',
+        format: winston.format.combine(
+            appendMetaInfo(),
+            winston.format.json(),
+            winston.format.timestamp()
+        ),
+        transports: [
+            new LogstashTransport(config.logstash)
+        ].concat(config.transports || [])
+    });
 }
 
-exports.LogstashTransport = LogstashTransport
-exports.createLogger = createLogger
+exports.LogstashTransport = LogstashTransport;
+exports.createLogger = createLogger;
